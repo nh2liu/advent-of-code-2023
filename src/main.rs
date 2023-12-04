d2aa13a2977cae1c5389416989e259f34c4d75f6
Neil Liu <neil.liu16@gmail.com>
2023-12-04 17:09:27 -0500

    day3 p1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day01_trebuchet;
 mod day02_cube_conundrum;
+mod day03_gear_ratios;
 
 mod utils;
 
@@ -22,6 +23,7 @@ fn main() {
         ("day01", &day01_trebuchet::Day01 as &dyn Solution),
         ("day02_1", &day02_cube_conundrum::Day02_1 as &dyn Solution),
         ("day02_2", &day02_cube_conundrum::Day02_2 as &dyn Solution),
+        ("day03_1", &day03_gear_ratios::Day03_1 as &dyn Solution),
     ]
     .iter()
     .cloned()
