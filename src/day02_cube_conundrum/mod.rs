ddaebbb39264f86992773114f9cec32366184245
Neil Liu <neil.liu16@gmail.com>
2023-12-04 14:02:19 -0500

    day02

---
+++ src/day02_cube_conundrum/mod.rs
@@ -0,0 +1,81 @@
+use std::collections::HashMap;
+
+//mod utils;
+use crate::utils::Solution;
+use std::cmp::max;
+pub struct Day02_1;
+pub struct Day02_2;
+
+fn parse_draw(draw: &str) -> HashMap<&str, u32> {
+    // 1 red, 2 green, 6 blue -> {"red": 1, "green": 2, "blue": 6}
+    return draw
+        .trim()
+        .split(", ")
+        .map(|cstr| cstr.split_once(" ").unwrap())
+        .map(|(n_str, c_str)| {
+            return (
+                c_str,
+                n_str.parse::<u32>().unwrap_or_else(|_| {
+                    println!("{} | {} | {}", draw, n_str, c_str);
+                    panic!("lol");
+                }),
+            );
+        })
+        .collect();
+}
+
+fn check_game(line: &String, condition: &HashMap<&str, u32>) -> bool {
+    let (_, line) = line.split_once(':').unwrap();
+    line.split(';').map(parse_draw).all(|marbles| {
+        marbles
+            .iter()
+            .all(|(key, val)| condition.contains_key(key) && condition.get(key).unwrap() >= val)
+    })
+}
+
+fn fewest_cubes(line: &String) -> u32 {
+    let (_, line) = line.split_once(':').unwrap();
+    let mut min_cubes: HashMap<&str, u32> = HashMap::new();
+    for draw in line.split(';').map(parse_draw) {
+        for (color, count) in draw.iter() {
+            min_cubes.insert(
+                color,
+                max(min_cubes.get(color).unwrap_or(&0), count).to_owned(),
+            );
+        }
+    }
+    return min_cubes.values().product();
+}
+
+impl Solution for Day02_1 {
+    fn name(&self) -> &str {
+        "day02_cube_conundrum"
+    }
+    fn solve(&self, lines: &Vec<String>) -> String {
+        let condition: HashMap<&str, u32> = [("red", 12), ("green", 13), ("blue", 14)]
+            .iter()
+            .cloned()
+            .collect();
+        return lines
+            .iter()
+            .enumerate()
+            .map(|(i, line)| {
+                if check_game(line, &condition) {
+                    i + 1
+                } else {
+                    0
+                }
+            })
+            .sum::<usize>()
+            .to_string();
+    }
+}
+
+impl Solution for Day02_2 {
+    fn name(&self) -> &str {
+        "day02_cube_conundrum"
+    }
+    fn solve(&self, lines: &Vec<String>) -> String {
+        return lines.iter().map(fewest_cubes).sum::<u32>().to_string();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,10 @@ mod day02_cube_conundrum;
 mod utils;
 
 use crate::utils::Solution;
+use inquire::Select;
+use itertools::Itertools;
 use std::collections::HashMap;
 use std::fs::read_to_string;
-
-use crate::day01_trebuchet::Day01;
-use crate::day02_cube_conundrum::Day02;
 
 fn read_lines(filename: &str) -> Vec<String> {
     return read_to_string(filename)
@@ -20,13 +19,20 @@ fn read_lines(filename: &str) -> Vec<String> {
 
 fn main() {
     let solutions_map: HashMap<&str, &dyn Solution> = [
-        ("day01", &Day01 as &dyn Solution),
-        ("day02", &Day02 as &dyn Solution),
+        ("day01", &day01_trebuchet::Day01 as &dyn Solution),
+        ("day02_1", &day02_cube_conundrum::Day02_1 as &dyn Solution),
+        ("day02_2", &day02_cube_conundrum::Day02_2 as &dyn Solution),
     ]
     .iter()
     .cloned()
     .collect();
-    let solution = solutions_map.get("day02").unwrap();
+    let mut select_box = Select::new(
+        "Select the problem:",
+        solutions_map.keys().sorted().collect(),
+    );
+    select_box.starting_cursor = solutions_map.len() - 1;
+    let problem = select_box.prompt();
+    let solution = solutions_map.get(problem.unwrap()).unwrap();
     let lines = read_lines(format!("./src/{}/input.txt", solution.name()).as_str());
     println!("{}", solution.solve(&lines));
 }
