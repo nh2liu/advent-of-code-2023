6df8dd25e535c78048be8681307ebed3b560955d
Neil Liu <neil.liu16@gmail.com>
2023-12-04 21:08:41 -0500

    refactor p1

--- src/day03_gear_ratios/mod.rs
+++ src/day03_gear_ratios/mod.rs
@@ -1,8 +1,14 @@
+use std::collections::HashSet;
+
 use crate::utils::Solution;
 
 pub struct Day03_1;
 
-fn is_adjacent_to_symbol(m: i32, n: i32, grid: &Vec<Vec<char>>) -> bool {
+fn is_symbol(c: char) -> bool {
+    !(c.is_digit(10) || c == '.')
+}
+
+fn get_adjacent_cells(m: usize, n: usize, grid: &Vec<Vec<char>>) -> Vec<(usize, usize)> {
     let deltas = vec![
         (-1, -1),
         (-1, 0),
@@ -13,21 +19,17 @@ fn is_adjacent_to_symbol(m: i32, n: i32, grid: &Vec<Vec<char>>) -> bool {
         (1, 0),
         (1, 1),
     ];
-    return deltas.iter().any(|(dy, dx)| -> bool {
-        let (y, x) = ((m + dy), (n + dx));
-        grid.len() as i32 > y
-            && y >= 0
-            && grid[y as usize].len() as i32 > x
-            && x >= 0
-            && ({
-                let adjc = grid[y as usize][x as usize];
-                !(adjc.is_digit(10) || adjc == '.')
-            })
-    });
+    return deltas
+        .iter()
+        .map(|(dy, dx)| (m as i32 + dy, n as i32 + dx))
+        .filter(|(y, x)| -> bool {
+            grid.len() as i32 > *y && *y >= 0 && grid[0].len() as i32 > *x && *x >= 0
+        })
+        .map(|(y, x)| (y as usize, x as usize))
+        .collect();
 }
 
-fn parse_num_at(m: usize, n: usize, grid: &Vec<Vec<char>>) -> (usize, u32) {
-    let mut n_digits = 0;
+fn parse_num_at(m: usize, n: usize, grid: &Vec<Vec<char>>) -> u32 {
     let mut num = 0;
     for i in n..grid[0].len() {
         let c = grid[m][i];
@@ -35,37 +37,50 @@ fn parse_num_at(m: usize, n: usize, grid: &Vec<Vec<char>>) -> (usize, u32) {
             break;
         }
         num = num * 10 + c.to_digit(10).unwrap();
-        n_digits += 1;
     }
-    return (n_digits as usize, num);
+    return num;
+}
+
+fn create_first_letter_grid(grid: &Vec<Vec<char>>) -> Vec<Vec<Option<usize>>> {
+    let m = grid.len();
+    let n = grid[0].len();
+    let mut matrix: Vec<Vec<Option<usize>>> = Vec::with_capacity(m);
+    for i in 0..m {
+        let mut row = Vec::with_capacity(n);
+        let mut start_idx = None;
+        for j in 0..n {
+            // Tracking the first index of the string.
+            if grid[i][j].is_digit(10) {
+                if start_idx.is_none() {
+                    start_idx = Some(j);
+                }
+                row.push(start_idx);
+            } else {
+                start_idx = None;
+                row.push(None);
+            }
+        }
+        matrix.push(row);
+    }
+    return matrix;
 }
 
 fn solve_grid(grid: &Vec<Vec<char>>) -> u32 {
-    let mut running_sum = 0;
+    let fl_grid = create_first_letter_grid(grid);
 
+    let mut parts = HashSet::new();
     for (m, row) in grid.iter().enumerate() {
-        let mut current_start_number_index = None;
-        let mut last_parsed_idx: i32 = -1;
         for (n, c) in row.iter().enumerate() {
-            if n as i32 <= last_parsed_idx {
-                continue;
-            }
-            if c.is_digit(10) {
-                if current_start_number_index.is_none() {
-                    current_start_number_index = Some(n);
+            if is_symbol(*c) {
+                for (i, j) in get_adjacent_cells(m, n, grid) {
+                    if fl_grid[i][j].is_some() {
+                        parts.insert((i, fl_grid[i][j].unwrap()));
+                    }
                 }
-                if is_adjacent_to_symbol(m as i32, n as i32, grid) {
-                    let sidx = current_start_number_index.unwrap();
-                    let (n_digits, sum) = parse_num_at(m, sidx, grid);
-                    last_parsed_idx = (sidx + n_digits - 1) as i32;
-                    running_sum += sum;
-                }
-            } else {
-                current_start_number_index = None;
             }
         }
     }
-    return running_sum;
+    return parts.iter().map(|(m, n)| parse_num_at(*m, *n, grid)).sum();
 }
 
 impl Solution for Day03_1 {
